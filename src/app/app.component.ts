3b665d4e26d28402072f2a689430610d564fcdba
b57zheng <b57zheng@uwaterloo.ca>
2022-07-11 19:04:47 -0400

    ERROR message logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent 
+{
   displayString = ""
   title = 'calculator-app';
   numbers = ['1', '2', '3', '4', '5', '6', '7','8','9','0']
@@ -13,39 +14,58 @@ export class AppComponent {
   splitNumbers: any[] = []
   splitOperators: any[] = []
   operators = ['+','-','x','/']
-  updateDisplay(input: string){
+  updateDisplay(input: string)
+  {
     this.displayString = this.displayString + input
   }
 
-  clearString(){
+  clearString()
+  {
     this.displayString = ""
   }
-  backspaceString(){
-      this.displayString = this.displayString.slice(0,this.displayString.length-1)
+  backspaceString()
+  {
+      if (this.displayString = 'ERROR')
+      {
+        this.displayString = ""
+      }
+      else
+      {
+        this.displayString = this.displayString.slice(0,this.displayString.length-1)
+      }
   }
-  calculate(){
+  calculate()
+  {
     this.splitNumbers = []
     this.splitOperators = []
     this.operatorIndex = []
     console.log(this.displayString.length)
     var index = 0
     var prevStringElement
-    for(let i=0; i<this.displayString.length; i++){ //iterating through string getting the index of the operators
-      if(this.operators.includes(this.displayString[i])){
+    //iterating through string getting the index of the operators
+    for(let i=0; i<this.displayString.length; i++)
+    { 
+      if(this.operators.includes(this.displayString[i]))
+      {
         this.operatorIndex.push(i)
         this.splitOperators.push(this.displayString[i])
-        if(prevStringElement == 'operator'){
+        if(prevStringElement == 'operator')
+        {
           return this.displayString = 'ERROR'
         }
         prevStringElement = 'operator'
       }
-      else{
+      else
+      {
         prevStringElement = 'number'
       }
     }
-    for(let i=0; i<this.operatorIndex.length; i++){ //splitting the string into numbers and operators
+    //splitting the string into numbers and operators
+    for(let i=0; i<this.operatorIndex.length; i++)
+    { 
       var split = this.displayString.slice(index, this.operatorIndex[i])
-      index = this.operatorIndex[i] + 1 //updating pointer
+      //updating pointer
+      index = this.operatorIndex[i] + 1 
       this.splitNumbers.push(split)
     }
     split = this.displayString.slice(index, this.displayString.length)
@@ -54,22 +74,34 @@ export class AppComponent {
     console.log(this.splitOperators)
     var outputNumber = Number(this.splitNumbers[0]) //setting stored variable to first item of array
     var splitNumberPointer = 1 //setting pointer to look at second element in the array
-    for(let i=0; i<this.splitOperators.length; i++){
-      if(this.splitOperators[i] == '+'){
+    for(let i=0; i<this.splitOperators.length; i++)
+    {
+      if(this.splitOperators[i] == '+')
+      {
         outputNumber = outputNumber + Number(this.splitNumbers[splitNumberPointer]) //operations on the first index and the number pointer pointing
         splitNumberPointer = splitNumberPointer + 1 //updating pointer
       }
-      if(this.splitOperators[i] == '-'){
+      if(this.splitOperators[i] == '-')
+      {
         outputNumber = outputNumber - Number(this.splitNumbers[splitNumberPointer])
         splitNumberPointer = splitNumberPointer + 1
       }
-      if(this.splitOperators[i] == 'x'){
+      if(this.splitOperators[i] == 'x')
+      {
         outputNumber = outputNumber * Number(this.splitNumbers[splitNumberPointer])
         splitNumberPointer = splitNumberPointer + 1
       }
-      if(this.splitOperators[i] == '/'){
+      if(this.splitOperators[i] == '/')
+      {
         outputNumber = outputNumber * 1/Number(this.splitNumbers[splitNumberPointer])
-        splitNumberPointer = splitNumberPointer + 1
+        if(this.splitNumbers[splitNumberPointer] == 0)
+        {
+          return this.displayString = 'ERROR'
+        }
+        else
+        {
+          splitNumberPointer = splitNumberPointer + 1
+        }
       }
 
     }
